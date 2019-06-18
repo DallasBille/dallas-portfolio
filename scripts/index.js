6a31f0655295da291eb7dfdf0eb7a6810725e349
github username <github email address>
2019-06-18 18:50:08 -0400

    fixed bug for contact page logo in nav

--- scripts/index.js
+++ scripts/index.js
@@ -55,3 +55,38 @@ navBarToggle.addEventListener("click", (event) => {
 // fetch('http://127.0.0.1:8000/api/stock/')
 //     .then(res => res.json())
 //     .then(stocks => console.log(stocks.objects))
+
+// =========== RECURSIVE FUNCTION
+// function printString(string){
+//     console.log(string[0]);
+//     debugger
+//     if(string.length > 1){
+//          string = string.substring(1,string.length)
+//         printString(string)
+//     } else {
+//         return true
+//     }
+//
+// }
+// printString("thisisastring")
+
+// function reverseString(string){
+    // your making a new string so you have to make an empty one.
+    // we need to bring everything at the end of the string to the front,
+    // so i = string.length - 1 is the last index. As long as i is greater than or equal to zero (the lowest index), decrement i. Each iteration, the last item is put first until after i = index 0.
+    // for(i = string.length-1; i>=0 ; i--){
+        // string += string[i]
+        // debugger
+    // }
+    // return newString
+// }
+
+function reverseString(str) {
+  return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+}
+console.log(reverseString("spaces between"))
+
+
+console.log("Welcome to my website!")
+console.log("I'm glad you could join me!")
+console.log("If you need a developer let me know, I'm always interested in an opportunity.")
